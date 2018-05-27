1261b4310ef195a474316e3b2d86f4f11681ff7b
Brandon Williams <BrandonWilliamsCS@gmail.com>
2018-05-27 14:55:19 -0400

    first attempt at bmp loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
 
-import logo from './logo.svg';
+import { BMPBitmap } from "bitmap-manipulation";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import "./App.css";
+import sampleImage from "./sample.bmp";
+
+interface AppState {
+    image: BMPBitmap | undefined;
+}
+
+class App extends React.Component<{}, AppState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            image: undefined,
+        };
+        this.loadImage = this.loadImage.bind(this);
+    }
+    public render() {
+        return (
+            <div className="App">
+                <button type="button" onClick={this.loadImage}>
+                    Load
+                </button>
+            </div>
+        );
+    }
+
+    private loadImage() {
+        this.setState({ image: BMPBitmap.fromFile(sampleImage) });
+    }
 }
 
 export default App;
